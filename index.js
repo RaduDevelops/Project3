2ca16ce5e3ddfcba820aefb0c4fc76e6f4c7f3f7
RaduDevelops <raducu_hogas@yahoo.com>
2022-09-24 18:59:33 +0300

    validating the email input :)

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+
+// Declaring the vars
 let error = document.getElementById("email-error");
 let button = document.getElementById("input-button");
-let value = document.getElementById("email-input").value;
-let regex =/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-button.addEventListener("click",()=>{
-     if(regex.test(value)){
-      error.classList.remove("hidden"); 
-    }
+let emailInput = document.getElementById("email-input");
+let regex =/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+// the validation function
+const validateEmail = () => {
+  if(!regex.test(emailInput.value)){
+     error.classList.remove("hidden")
+  }
+  console.log(regex.test(emailInput.value));
+};
+// adding the event listeners 
+button.addEventListener("click",(e)=>{
+  validateEmail();
 });
-
-
+emailInput.addEventListener("input",() => 
+ error.classList.add("hidden")
+);
